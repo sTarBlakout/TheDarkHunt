8e0288697a977c6d4c548725d981fcc888a619ee
sTarBlakout <46494422+sTarBlakout@users.noreply.github.com>
2020-07-18 21:48:20 +0200

    Movement and rotation added.
    Also added some prototype assets.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TheDarkHunt.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private FixedJoystick _fixedJoystick;
+
+        private float _inputX;
+        private float _inputZ;
+
+        public float InputX => _inputX;
+        public float InputZ => _inputZ;
+        
+        private void Awake()
+        {
+            _fixedJoystick = FindObjectOfType<FixedJoystick>();
+        }
+
+        private void Update()
+        {
+            GrabInput();
+        }
+
+        private void GrabInput()
+        {
+            _inputX = _fixedJoystick.Horizontal;
+            _inputZ = _fixedJoystick.Vertical;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TheDarkHunt.Player
+{
+    public class PlayerMover : MonoBehaviour
+    {
+        [SerializeField] private float moveSpeedCoef = 1f;
+        [SerializeField] private float rotateSpeedCoef = 1f;
+        
+        private CharacterController _characterController;
+        private PlayerController _playerController;
+
+        private Vector3 _movement;
+        private Quaternion _rotation;
+
+        private void Awake()
+        {
+            _characterController = GetComponent<CharacterController>();
+            _playerController = GetComponent<PlayerController>();
+        }
+
+        private void FixedUpdate()
+        {
+            UpdateMovement();
+            UpdateRotation();
+        }
+        
+        private void UpdateMovement()
+        {
+            _movement = new Vector3(_playerController.InputX, 0f, _playerController.InputZ);
+            _characterController.Move(_movement * moveSpeedCoef);
+        }
+        
+        private void UpdateRotation()
+        {
+            if (_movement == Vector3.zero) return;
+            
+            var rotateTo = Quaternion.LookRotation(_movement);
+            _rotation = Quaternion.RotateTowards(transform.rotation, rotateTo, rotateSpeedCoef * Time.deltaTime);
+            transform.rotation = _rotation;
+        }
+    }
+}
