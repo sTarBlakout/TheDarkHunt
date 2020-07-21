cccf89d70049f5905613c7bc7873d86ce3230afb
sTarBlakout <46494422+sTarBlakout@users.noreply.github.com>
2020-07-21 23:36:59 +0200

    Added first weapon SO.

---
+++ Assets/Scripts/Weapons/MeleeWeapons/FistWeapon.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Weapons.MeleeWeapons
+{
+    [CreateAssetMenu(menuName = "Melee Weapons/Fist Weapon", order = 1)]
+    public class FistWeapon : MeleeWeaponBase
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Weapons/MeleeWeapons/MeleeWeaponBase.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Weapons.MeleeWeapons
+{
+    public class MeleeWeaponBase : ScriptableObject
+    {
+        [SerializeField] private float damage;
+        [SerializeField] private float attackSpeed;
+
+        public float Damage => damage;
+        public float AttackSpeed => attackSpeed;
+    }
+}
