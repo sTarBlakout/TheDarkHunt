020efbd0dfb979684a629bddb1511b1dd15641a2
sTarBlakout <46494422+sTarBlakout@users.noreply.github.com>
2020-07-21 23:51:54 +0200

    Added some fields to MeleeWeaponBase

--- Assets/Scripts/Weapons/MeleeWeapons/MeleeWeaponBase.cs
+++ Assets/Scripts/Weapons/MeleeWeapons/MeleeWeaponBase.cs
@@ -4,9 +4,14 @@ namespace Weapons.MeleeWeapons
 {
     public class MeleeWeaponBase : ScriptableObject
     {
+        [Header("Graphics")] 
+        [SerializeField] private GameObject weaponPrefab;
+            
+        [Header("Base Stats")]
         [SerializeField] private float damage;
         [SerializeField] private float attackSpeed;
 
+        public GameObject WeaponPrefab => weaponPrefab;
         public float Damage => damage;
         public float AttackSpeed => attackSpeed;
     }
