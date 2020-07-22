6f91cb3d811c266f0d6f2e58c3f9eb916dd22aeb
sTarBlakout <46494422+sTarBlakout@users.noreply.github.com>
2020-07-22 21:21:09 +0200

    Changed inheritance structure of weapons.

--- Assets/Scripts/Weapons/MeleeWeapons/MeleeWeaponBase.cs
+++ Assets/Scripts/Weapons/MeleeWeapons/MeleeWeaponBase.cs
@@ -2,16 +2,12 @@
 
 namespace Weapons.MeleeWeapons
 {
-    public class MeleeWeaponBase : ScriptableObject
+    public class MeleeWeaponBase : WeaponBase
     {
-        [Header("Graphics")] 
-        [SerializeField] private GameObject weaponPrefab;
-            
         [Header("Base Stats")]
         [SerializeField] private float damage;
         [SerializeField] private float attackSpeed;
-
-        public GameObject WeaponPrefab => weaponPrefab;
+        
         public float Damage => damage;
         public float AttackSpeed => attackSpeed;
     }
---
+++ Assets/Scripts/Weapons/WeaponBase.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Weapons
+{
+    public class WeaponBase : ScriptableObject
+    {
+        [Header("Graphics")] 
+        [SerializeField] private GameObject weaponPrefab;
+
+        public GameObject WeaponPrefab => weaponPrefab;
+    }
+}
